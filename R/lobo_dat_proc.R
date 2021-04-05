45bfc6828ca4076f4bea628adf250d183c7353bc
fawda123 <mbafs2012@gmail.com>
2021-04-04 20:23:00 -0400

    lobo data processing

---
+++ R/lobo_dat_proc.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(janitor)
+library(lubridate)
+
+dt <- Sys.Date()
+url <- paste('http://tampabay.loboviz.com/cgi-data/nph-data.cgi?node=82&min_date=20210328&max_date=', dt, '&y=salinity,temperature,co2,oxygen,pht,par,pressure&data_format=text')
+
+datraw <- read.table(url, skip = 2, sep = '\t', header = T) %>% 
+  clean_names() %>% 
+  select(
+    datetime = date_est, 
+    sal = salinity_psu, 
+    temp = temperature_c, 
+    co2 = co2_ppm, 
+    do = dissolved_oxygen_mg_l, 
+    ph = p_ht, 
+    par = par_u_m_m_2_sec,
+    press = pressure_d_bar
+  ) %>% 
+  mutate(
+    datetime = ymd_hms(datetime, tz = 'America/Jamaica') 
+  )
+
+save(datraw, file = 'data/lobo.RDtaa', version = 2)
