499273bfca97718b5d1a3fb579f23bda3cb756a8
fawda123 <mbafs2012@gmail.com>
2021-04-05 15:03:46 -0400

    change to y axis font label size, fix to source/social links in banner

--- R/funcs.R
+++ R/funcs.R
@@ -19,41 +19,43 @@ plo_fun <- function(wqdat, station, cols){
   toplo <- wqdat %>% 
     filter(station %in% !!station) %>% 
     spread(var, val)
-
+  
+  ylbsz <- 18
+  
   p1 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~tp, type = 'scatter', color = I(cols[6]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = 'TP (mg/L)', range = rngs$tp)
+      yaxis = list(title = 'TP (mg/L)', range = rngs$tp, titlefont = list(size = ylbsz))
       )
 
   p2 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~tn, type = 'scatter', color = I(cols[5]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = 'TN (mg/L)', range = rngs$tn)
+      yaxis = list(title = 'TN (mg/L)', range = rngs$tn, titlefont = list(size = ylbsz))
       )
   
   p3 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~nh3, type = 'scatter', color = I(cols[4]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = 'Ammonia (mg/L)', range = rngs$nh3)
+      yaxis = list(title = 'NH3 (mg/L)', range = rngs$nh3, titlefont = list(size = ylbsz))
     )  
   
   p4 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~chla, type = 'scatter', color = I(cols[3]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = 'Chl-a (ug/L)', range = rngs$chla)
+      yaxis = list(title = 'Chl-a (ug/L)', range = rngs$chla, titlefont = list(size = ylbsz))
       )  
     
   p5 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~ph, type = 'scatter', color = I(cols[2]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = 'pH', range = rngs$ph)
+      yaxis = list(title = 'pH', range = rngs$ph, titlefont = list(size = ylbsz))
       )  
     
   p6 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~sal, type = 'scatter', color = I(cols[1]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = 'Salinity (ppt)', range = rngs$sal)
+      yaxis = list(title = 'Sal (ppt)', range = rngs$sal, titlefont = list(size = ylbsz))
       )
 
   p <- subplot(p1, p2, p3, p4, p5, p6, nrows = 6, shareX = T, shareY = F, titleY = T) %>% 
