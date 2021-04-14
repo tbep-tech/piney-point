0502fad42060df1f6aca6b6a289535ee6e00f55c
fawda123 <mbafs2012@gmail.com>
2021-04-14 11:35:40 -0400

    added 2021 sg data to baseline

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -485,7 +485,7 @@ fls <- drive_ls(gdrive_pth, type = 'spreadsheet')
 
 ##
 # fldep dump 20210411
-fl <- fls[which(fls$name == 'FLDEP_20210413'), 'id'] %>% pull(id)
+fl <- fls[which(fls$name == 'FLDEP_20210414'), 'id'] %>% pull(id)
 flsht <- read_sheet(fl)
 fldep1 <- flsht %>% 
   clean_names %>% 
