a9b7a38d630f8f02748f43854f94dfbfb49fccb3
fawda123 <mbafs2012@gmail.com>
2021-05-05 20:19:02 -0400

    fix hover label on baseline map, no fixed y axis ranges on baseline plot

--- R/funcs.R
+++ R/funcs.R
@@ -30,37 +30,37 @@ plo_fun <- function(wqdat, station, cols, parms){
   p1 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~tp, type = 'scatter', color = I(cols[6]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = lbs['tp'], range = rngs$tp, titlefont = list(size = ylbsz))
+      yaxis = list(title = lbs['tp'], titlefont = list(size = ylbsz))#, range = rngs$tp)
       )
 
   p2 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~tn, type = 'scatter', color = I(cols[5]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = lbs['tn'], range = rngs$tn, titlefont = list(size = ylbsz))
+      yaxis = list(title = lbs['tn'], titlefont = list(size = ylbsz))#, range = rngs$tn)
       )
   
   p3 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~nh34, type = 'scatter', color = I(cols[4]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = lbs['nh34'], range = rngs$nh34, titlefont = list(size = ylbsz))
+      yaxis = list(title = lbs['nh34'], titlefont = list(size = ylbsz))#, range = rngs$nh34)
     )  
   
   p4 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~chla, type = 'scatter', color = I(cols[3]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = lbs['chla'], range = rngs$chla, titlefont = list(size = ylbsz))
+      yaxis = list(title = lbs['chla'], titlefont = list(size = ylbsz))#, range = rngs$chla)
       )  
     
   p5 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~ph, type = 'scatter', color = I(cols[2]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = lbs['ph'], range = rngs$ph, titlefont = list(size = ylbsz))
+      yaxis = list(title = lbs['ph'], titlefont = list(size = ylbsz))#, range = rngs$ph)
       )  
     
   p6 <- plot_ly(toplo) %>% 
     add_markers(x = ~date, y = ~sal, type = 'scatter', color = I(cols[1]), mode = 'markers', line = list(shape = 'linear'), showlegend = F) %>% 
     layout(
-      yaxis = list(title = lbs['sal'], range = rngs$sal, titlefont = list(size = ylbsz))
+      yaxis = list(title = lbs['sal'], titlefont = list(size = ylbsz))#, range = rngs$sal)
       )
 
   p <- subplot(p1, p2, p3, p4, p5, p6, nrows = 6, shareX = T, shareY = F, titleY = T) %>% 
