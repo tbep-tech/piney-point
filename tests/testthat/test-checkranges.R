7e7cc4a0608b983d553241ad421d6d9922e96d25
fawda123 <mbafs2012@gmail.com>
2021-05-07 07:48:56 -0400

    better test for check ranges on baseline comparison

--- tests/testthat/test-checkranges.R
+++ tests/testthat/test-checkranges.R
@@ -1,6 +1,6 @@
 test_that("Check if there are ranges for comparison for all sites", {
   
-  chk <- any(is.na(rswqdat$nrmrng))
+  chk <- any(grepl('NA', rswqdat$nrmrng))
   
   expect_false(chk)
   
