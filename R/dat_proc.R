74dd03010ef47dcb4469281e3d60bcfc46f929a6
fawda123 <mbafs2012@gmail.com>
2021-05-18 17:39:14 -0400

    fldep dump

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1068,7 +1068,7 @@ fls <- drive_ls(gdrive_pth, type = 'spreadsheet')
 
 ## fldep ------------------------------------------------------------------
 
-fl <- fls[which(fls$name == 'FLDEP_20210514'), 'id'] %>% pull(id)
+fl <- fls[which(fls$name == 'FLDEP_20210518'), 'id'] %>% pull(id)
 flsht <- read_sheet(fl)
 fldep1 <- flsht %>% 
   clean_names %>% 
