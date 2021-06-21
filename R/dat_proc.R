8498272b59899ef5d8ff93c613a6fee07764e6ee
fawda123 <mbafs2012@gmail.com>
2021-06-21 17:26:34 -0400

    save file

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1261,7 +1261,7 @@ fls <- drive_ls(gdrive_pth, type = 'spreadsheet')
 
 ## fldep ------------------------------------------------------------------
 
-fl <- fls[which(fls$name == 'FLDEP_20210620'), 'id'] %>% pull(id)
+fl <- fls[which(fls$name == 'FLDEP_20210621'), 'id'] %>% pull(id)
 flsht <- read_sheet(fl)
 fldep1 <- flsht %>% 
   clean_names %>% 
