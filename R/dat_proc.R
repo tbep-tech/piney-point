cfe5573c1c9c85a12d1986763c6c63f5c2a40fdd
fawda123 <mbafs2012@gmail.com>
2021-07-01 08:30:54 -0400

    fwri phyto update

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1265,7 +1265,7 @@ fls <- drive_ls(gdrive_pth, type = 'spreadsheet')
 
 ## fldep ------------------------------------------------------------------
 
-fl <- fls[which(fls$name == 'FLDEP_20210625'), 'id'] %>% pull(id)
+fl <- fls[which(fls$name == 'FLDEP_20210701'), 'id'] %>% pull(id)
 flsht <- read_sheet(fl)
 fldep1 <- flsht %>% 
   clean_names %>% 
