86f19e259db807dbeaa468e30b584768118b7ce4
fawda123 <mbafs2012@gmail.com>
2021-08-18 12:47:05 -0400

    checking for fldep wq update

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1458,7 +1458,7 @@ fls <- drive_ls(gdrive_pth, type = 'spreadsheet')
 
 ## fldep ------------------------------------------------------------------
 
-fl <- fls[which(fls$name == 'FLDEP_20210813'), 'id'] %>% pull(id)
+fl <- fls[which(fls$name == 'FLDEP_20210818'), 'id'] %>% pull(id)
 flsht <- read_sheet(fl)
 fldep1 <- flsht %>% 
   clean_names %>% 
