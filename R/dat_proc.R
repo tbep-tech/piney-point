8728e2cae40874e46423ad21b657a970faba31db
fawda123 <mbafs2012@gmail.com>
2021-11-17 09:25:34 -0500

    checking dep wq data update

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -1786,7 +1786,7 @@ fls <- drive_ls(gdrive_pth, type = 'spreadsheet')
 
 ## fldep ------------------------------------------------------------------
 
-fl <- fls[which(fls$name == 'FLDEP_20211017'), 'id'] %>% pull(id)
+fl <- fls[which(fls$name == 'FLDEP_20211117'), 'id'] %>% pull(id)
 flsht <- read_sheet(fl)
 out1 <- flsht %>% 
   clean_names %>% 
@@ -2779,11 +2779,11 @@ cosp1 <- out1
 
 ## combine all ------------------------------------------------------------
 
-# rswqdat <- rswqdat %>%
-#   select(station, date, source, var, uni, val, qual) %>%
-#   filter(!source %in% c('fldep', 'epchc'))
-# rswqdat <- bind_rows(fldep1, epc1, rswqdat) %>%
-rswqdat <- bind_rows(fldep1, mpnrd1, pinco1, ncf1, epc1, esa1, usf1, uf1, cosp1) %>%
+rswqdat <- rswqdat %>%
+  select(station, date, source, var, uni, val, qual) %>%
+  filter(!source %in% c('fldep'))
+rswqdat <- bind_rows(fldep1, rswqdat) %>%
+# rswqdat <- bind_rows(fldep1, mpnrd1, pinco1, ncf1, epc1, esa1, usf1, uf1, cosp1) %>%
   ungroup %>% 
   unique %>%
   filter(!is.na(val)) %>%
